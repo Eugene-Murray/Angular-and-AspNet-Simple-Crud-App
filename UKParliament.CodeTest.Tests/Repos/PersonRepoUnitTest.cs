8e33015e96aa17081dcecab8f04acfd6487a4dd6
Eugene Murray <eugene.murray@gmail.com>
2025-03-12 20:20:54 +0000

    Add unit tests

---
+++ UKParliament.CodeTest.Tests/Repos/PersonRepoUnitTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using UKParliament.CodeTest.Data;
+using UKParliament.CodeTest.Data.Entities;
+using UKParliament.CodeTest.Data.Repositories;
+using UKParliament.CodeTest.Services;
+using UKParliament.CodeTest.Services.Mappers;
+using UKParliament.CodeTest.Tests.Repos;
+
+namespace UKParliament.CodeTest.Tests.Services
+{
+
+    public class PersonRepoUnitTest : RepoBaseUnitTest
+    {
+        [Fact]
+        public void Should_CreatePerson_Entity_List()
+        {
+            // Arrange
+            var options = base.GetInMemoryDbContextOptions();
+            var repo = new PersonRepository(new PersonManagerContext(options));
+
+            // Act
+            var result = repo.Create(new Person { FirstName = "Test Name", LastName = "", DepartmentId = 2, DOB = new DateOnly(2000, 1, 1) });
+
+            // Assert
+            Assert.Equal(result.FirstName, "Test Name");
+        }
+    }
+}
---
+++ UKParliament.CodeTest.Tests/Repos/RepoBaseUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKParliament.CodeTest.Data;
+
+namespace UKParliament.CodeTest.Tests.Repos
+{
+    public class RepoBaseUnitTest
+    {
+        public DbContextOptions<PersonManagerContext> GetInMemoryDbContextOptions()
+        {
+            var options = new DbContextOptionsBuilder<PersonManagerContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            return options;
+        }
+    }
+}
